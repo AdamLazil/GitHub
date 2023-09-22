ae4467c1a1a1a2ecac167c68af54cd5710717954
AdamLazil <lizaladam@seznam.cz>
2023-09-22 13:28:25 +0200

    zmena prikazu

---
+++ projectGitHub.py
@@ -0,0 +1,10 @@
+motorCycles = []
+motorCycles.append("Surron")
+motorCycles.append("Surron L1x")
+print(motorCycles)
+
+motorCycles.insert(0, "Surron Ultra Bee")
+print(motorCycles)
+
+motorCycles.pop(1)
+print(motorCycles)
